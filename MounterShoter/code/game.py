3cb698d16453175b51d4af1b041bd5076ed7b39e
Antônio Marcos <antoniomarcosddsj@gmail.com>
2025-03-23 20:06:29 -0300

    Create Menu Image Linked to Menu Added Const Added Colors
    pt-br
    
    Menu criar
    Imagem vinculada ao menu
    Adicionado Const
    Adicionado cores

--- MounterShoter/code/game.py
+++ MounterShoter/code/game.py
@@ -2,21 +2,18 @@
 # -*- coding: utf-8 -*-
 import pygame
 
+from code.Const import WINDOW_WIDTH, WINDOW_HEIGHT
 from code.Menu import Menu
 
 
 class Game:
     def __init__(self):
         pygame.init()
-        self.window = pygame.display.set_mode(size=(600, 480)) #tamanho da janela do jogo
+        self.window = pygame.display.set_mode(size=(WINDOW_WIDTH, WINDOW_HEIGHT))  # tamanho da janela do jogo
 
-    def run(self,window ):
+    def run(self):
+
         while True:
-            menu = Menu(self.window) # Aqui estou chamando o menu do jogo
-            menu = menu.run()
-
-        # Check for all events (check todos os eventos)
-        #for event in pygame.event.get():
-            #if event.type == pygame.QUIT:
-               # pygame.quit()  # close window (fecha a janela)
-                #quit()  # end pygame (finaliza o pygame)
+            menu = Menu(self.window)  # Aqui estou chamando o menu do jogo
+            menu.run()
+            pass
